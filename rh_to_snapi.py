fd352296915bbb108c324a7143c8ff8e3233ad48
milic <ivanzmilic@gmail.com>
2025-10-17 17:16:30 +0200

    Post meeting. Planning to use a different FALC model and to debug more

---
+++ rh_to_snapi.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# The function reads an RH model atmosphere and outputs a SNAPI model atmosphere
+# RH has no magnetic fields so that will be zero 
+# RH only has electrons, hydrogen populations but we need pressure. 
+
